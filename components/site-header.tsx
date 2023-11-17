8e5060af61de6d0b62d560f79ca1744f3d91cc6a
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-11-16 21:00:13 -0500

    refactor: enhanced navbar visibility by implementing dynamic style change on scroll

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,15 +1,43 @@
+"use client";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-import { BrainCog } from "lucide-react";
+import { Icons } from "./icons";
+import { useEffect, useRef } from "react";
 
 export default function SiteHeader() {
+  const ref = useRef<HTMLHeadElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 10) {
+        ref.current?.classList.add(
+          "border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+        );
+      } else {
+        ref.current?.classList.remove(
+          "border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+        );
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="sticky top-0 w-screen h-20 flex justify-between items-center px-4 md:px-6">
+    <header
+      ref={ref}
+      className="sticky top-0 w-screen h-20 flex justify-between items-center px-4 md:px-6"
+    >
       <Link
         href="/"
         className="w-8 h-10 inline-flex items-center justify-center"
       >
-        <BrainCog className="w-6 h-6" />
+        <Icons.logo className="w-6 h-6" />
       </Link>
       <nav>
         <Link
