dbabc49e8e944d863686daddcbb9c6e731be4c39
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-11-16 21:37:59 -0500

    feat: toaster provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import SiteHeader from "@/components/site-header";
 import SiteFooter from "@/components/site-footer";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <SiteHeader />
         {children}
+        <Toaster />
         <SiteFooter />
       </body>
     </html>
